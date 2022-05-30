f42b6449ee811c364c0b653e18786433b4032142
Alair <alair.toxtega@gmail.com>
2022-05-30 10:18:44 -0500

    intentoCargaJSON

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-while True:
-    print("AAAAAAAAAAAAA")
+from importlib.metadata import FileHash
+import json
+
+def cargarArchivo(rutaArchivo):
+    with open(rutaArchivo) as filas:
+        contenido = json.load(filas)
+        print(contenido.intento)
+
+rutaArchivo = 'laberinto.json'
