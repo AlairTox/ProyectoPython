c336cc6b752c335f06463e39c5f3d3de87c87605
Alair <alair.toxtega@gmail.com>
2022-05-30 11:10:19 -0500

    LecturaInicialJSON

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-
 import json
 
 def cargarArchivo(rutaArchivo):
-    with open(rutaArchivo) as filas:
-        contenido = json.load(filas)
-        print(contenido)
+    with open(rutaArchivo) as archivo:
+        contenido = json.load(archivo)
+        print(type(contenido))
+        for datos in contenido:
+            print(datos.get("intento"))
 
 rutaArchivo = 'laberinto.json'
+
+cargarArchivo(rutaArchivo)
